aab4d2ab59806a460ea519b3110463a4c76ec8a2
Akash Verma <akashv1507@gmai.com>
2021-09-09 12:05:27 +0530

    log file added

--- runDa.bat
+++ runDa.bat
@@ -1,2 +1,3 @@
+call project_env\Scripts\activate.bat
 call python index_daForecastRepGenerator.py >>DA_log.txt 2>&1
 call python index_daNldcSftpTransfer.py >>DA_log.txt 2>&1
--- runIntraday.bat
+++ runIntraday.bat
@@ -1,2 +1,3 @@
+call project_env\Scripts\activate.bat
 call python index_intradayForecastRepGenerator.py >>intraday_log.txt 2>&1
 call python index_intradayNldcSftpTransfer.py >>intraday_log.txt 2>&1
